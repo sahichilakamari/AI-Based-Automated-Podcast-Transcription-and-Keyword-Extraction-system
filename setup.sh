9930d75b6b03dfceff08921c2fa4697a3e7a006a
sahi <123293234+sahichilakamari@users.noreply.github.com>
2025-07-09 16:30:34 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ chmod -R 777 uploads temp outputs model_cache
 
 # Install Python dependencies with exact versions
 python -m pip install --upgrade pip
-python -m pip install -r requirements.txt --no-cache-dir --extra-index-url https://download.pytorch.org/whl/cu121
+python -m pip install -r requirements.txt --no-cache-dir
